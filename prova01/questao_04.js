0568177b6cfb809b7c55e5a7bb580d642b5dd1e8
Sergio Augusto <sacbj@icomp.ufam.edu.br>
2022-04-11 09:50:43 -0400

    questoes e arquivos para primeira avalizaçao de PW2022

---
+++ prova01/questao_04.js
@@ -0,0 +1,22 @@
+const fs = require("fs").promises;
+
+async function somar(){
+    const filesPath = ['./1.txt', './2.txt', './3.txt']
+    let soma = 0;
+    
+    for(index in filesPath){
+        const data = await fs.readFile(filesPath[index]).catch(err=>{
+            console.log(err)
+        })
+    
+        if(!data){
+            return;
+        }
+    
+        soma += parseInt(data);
+    }
+    
+    console.log(soma);
+}
+
+somar()
